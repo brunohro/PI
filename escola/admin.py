cbfcc4dfad0affae9df2b89a83d490c861a04051
brunohro <bh084996044995@gmail.com>
2024-03-19 08:21:50 -0300

    Criando as tabelas de Aluno e Cidade e as relacionando

--- escola/admin.py
+++ escola/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Professor, Disciplina
+from .models import Professor, Disciplina, Aluno, Cidade
 
+admin.site.register(Cidade)
+admin.site.register(Aluno)
 admin.site.register(Professor)
 admin.site.register(Disciplina)
--- escola/models.py
+++ escola/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
+class Cidade(models.Model):
+    nome = models.CharField(max_length=255)
+    sigla = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.nome
+
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField()
+    cidade = models.ForeignKey(Cidade, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+
 class Professor(models.Model):
     nome = models.CharField(max_length=255)
     email = models.EmailField()
